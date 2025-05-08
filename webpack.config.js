ef05b35fd8faf532ce65d971cbd4d37f173fa296
Ihor Ivaniuk <ibitlab@users.noreply.github.com>
2025-05-08 14:16:30 +0300

    adding tools: linter, formatter, pre commit hooks

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,30 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: "./src/index.js",
   output: {
-    // path: path.resolve(__dirname, "dist"),
-    path: path.resolve(__dirname, "public"),
+    path: path.resolve(__dirname, "build"),
     filename: "bundle.js",
   },
   mode: "development",
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, use: "babel-loader" },
+      { test: /\.css$/, use: [MiniCssExtractPlugin.loader, "css-loader"] },
+      { test: /\.(png|jpg|gif)$/, use: "file-loader" },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: "./public/index.html" }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({ filename: "styles.css" }),
+  ],
   devServer: {
-    static: path.resolve(__dirname, "public"),
+    static: path.resolve(__dirname, "build"),
     port: 3000,
-    open: true, // Automatically opens the browser
+    open: true,
   },
 };
